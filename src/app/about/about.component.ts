8ff34eda75d13f2dfc4e03a233341b1be984e9ca
shivendra8009727256 <monuy8009727256@gmail.com>
2025-02-25 17:19:05 +0530

    add image slide and responcive image and cards

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  ngAfterViewInit(): void {
+    const slideshowImages = document.querySelectorAll<HTMLImageElement>(".intro-slideshow img");
+
+    const nextImageDelay = 5000;
+    let currentImageCounter = 0;
+
+    // Ensure at least one image exists
+    if (slideshowImages.length > 0) {
+      slideshowImages[currentImageCounter].style.opacity = "1";
+
+      setInterval(() => {
+        slideshowImages[currentImageCounter].style.opacity = "0";
+        currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+        slideshowImages[currentImageCounter].style.opacity = "1";
+      }, nextImageDelay);
+    }
+  }
+
+   img1="/logo3.png"
+   img2="/best_logo1.png"
+   img3="/logo3.png"
+   img4="/opasLogo.png"
+   img5="/logo3.png"
+ 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button'
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  ngAfterViewInit(): void {
+  const slideshowImages = document.querySelectorAll<HTMLImageElement>(".intro-slideshow img");
+
+    const nextImageDelay = 5000;
+    let currentImageCounter = 0;
+
+    // Ensure at least one image exists
+    if (slideshowImages.length > 0) {
+      slideshowImages[currentImageCounter].style.opacity = "1";
+
+      setInterval(() => {
+        slideshowImages[currentImageCounter].style.opacity = "0";
+        currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+        slideshowImages[currentImageCounter].style.opacity = "1";
+      }, nextImageDelay);
+    }
+  }
+
+   img1="/logo3.png"
+   img2="/best_logo1.png"
+   img3="/logo3.png"
+   img4="/opasLogo.png"
+   img5="/logo3.png"
 
 }

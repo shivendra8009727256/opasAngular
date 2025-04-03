487999b4e0a0c84f3ce67d4f9156d9e68fafbca9
shivendra8009727256 <monuy8009727256@gmail.com>
2025-04-03 15:56:22 +0530

    add company logo in bottom

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })

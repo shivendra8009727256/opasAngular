1d65702777e88c02e4ff9f28cec45690079dfcae
shivendra8009727256 <monuy8009727256@gmail.com>
2025-05-09 17:02:19 +0530

    add privacy component

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.css'
 })
